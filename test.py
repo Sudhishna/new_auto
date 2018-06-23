88245cc28f5d94ba8980a2fcce1191f1f7e5c497
Sudhishna <ssendhil@juniper.net>
2018-06-22 17:10:08 -0700

    first commit

--- test.py
+++ test.py
@@ -44,9 +44,14 @@ complete = 0
 step1 = 0
 step2 = 0
 step3 = 0
+central_infra_services = 0
+regional_infra_services = 0
+central_micro_services = 0
+regional_micro_services = 0
+load_services_data = 0
 while True:
     if channel.recv_ready():
-        channel_data += channel.recv(100)
+        channel_data = channel.recv(9999)
         print channel_data
     else:
         continue
@@ -164,7 +169,42 @@ while True:
         with open('/root/passwords.txt','w') as f:
             f.write(content)
         print "###########################"
-        break
+
+
+    ansi_escape = re.compile(r'\x1B\[[0-?]*[ -/]*[@-~]')
+    channel_data = ansi_escape.sub('', channel_data)
+
+    if channel_data.endswith('installervm:~# '):
+        if step1 == 0:
+            channel.send('cd /root/Contrail_Service_Orchestration_3.3.1/\n')
+            step1 += 1
+
+    if channel_data.endswith('installervm:~/Contrail_Service_Orchestration_3.3.1# '):
+        if central_infra_services == 0:
+            #channel.send('DEPLOYMENT_ENV=central ./deploy_infra_services.sh\n')
+            central_infra_services += 1
+
+    if channel_data.endswith('installervm:~/Contrail_Service_Orchestration_3.3.1# '):
+        if central_infra_services == 1 and regional_infra_services == 0:
+            #channel.send('DEPLOYMENT_ENV=regional ./deploy_infra_services.sh\n')
+            regional_infra_services += 1
+
+    if channel_data.endswith('installervm:~/Contrail_Service_Orchestration_3.3.1# '):
+        if central_infra_services == 1 and regional_infra_services == 1 and central_micro_services == 0:
+            #channel.send('DEPLOYMENT_ENV=central ./deploy_micro_services.sh\n')
+            central_micro_services += 1
+
+    if channel_data.endswith('installervm:~/Contrail_Service_Orchestration_3.3.1# '):
+        if central_infra_services == 1 and regional_infra_services == 1 and central_micro_services == 1 and regional_micro_services == 0:
+            #channel.send('DEPLOYMENT_ENV=regional ./deploy_micro_services.sh\n')
+            regional_micro_services += 1
+
+    if channel_data.endswith('installervm:~/Contrail_Service_Orchestration_3.3.1# '):
+        if central_infra_services == 1 and regional_infra_services == 1 and central_micro_services == 1 and regional_micro_services == 1:
+            channel.send('./load_services_data.sh\n')
+            load_services_data += 1
+            break
+
 
 
  
